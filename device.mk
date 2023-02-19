fb116c5f440541701db0a2c129b8eca3b8e3c69a
pt20912 <122719561+pt20912@users.noreply.github.com>
2023-02-19 12:01:28 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,1 +1,12 @@
 
+LOCAL_PATH := device/samsung/m51
+
+TW_INCLUDE_FASTBOOTD := true
+
+PRODUCT_PACKAGES += \
+	fastbootd \
+	android.hardware.fastboot@1.0-impl-mock \
+	android.hardware.fastboot@1.0-impl-mock.recovery
+
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.fastbootd.available=true
